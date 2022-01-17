3f2ecd17c6011b5aad11841ab8f9e21caa81914e
Angel Caballero Dominguez <56036551+angcabdom@users.noreply.github.com>
2022-01-17 16:51:52 +0100

    Solve merge local conflict

--- microservices/frontend/src/modules/home/HomePage.js
+++ microservices/frontend/src/modules/home/HomePage.js
@@ -1,12 +1,14 @@
 import { Dashboard } from './components/Dashboard';
 import { ArtworkPage } from '../artwork/ArtworkPage';
 import { Routes, Route } from 'react-router-dom';
+import { OrderPage } from '../order/OrderPage';
 
 export function HomePage () {
   return (
     <Routes>
       <Route exact path='/' element={<Dashboard />} />
       <Route path='/artwork/:slug' element={<ArtworkPage />} />
+      <Route path='/order/:slug' element={<OrderPage />} />
     </Routes>
   );
 }
