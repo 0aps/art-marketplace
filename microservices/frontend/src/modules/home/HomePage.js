b2372d0bb290dac78e8555b7730eb989b40f2d8e
Angel Caballero Dominguez <56036551+angcabdom@users.noreply.github.com>
2022-01-18 17:34:47 +0100

    Fix HomePage duplication

--- microservices/frontend/src/modules/home/HomePage.js
+++ microservices/frontend/src/modules/home/HomePage.js
@@ -2,7 +2,6 @@ import { Dashboard } from './components/Dashboard';
 import { ArtworkPage } from '../artwork/ArtworkPage';
 import { OrderPage } from '../order/OrderPage';
 import { Routes, Route } from 'react-router-dom';
-import { OrderPage } from '../order/OrderPage';
 
 export function HomePage () {
   return (
