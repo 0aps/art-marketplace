84aa05e73caefe69a9d0902e1461302fff672d75
Ángel Piña <aps--@hotmail.com>
2022-01-18 02:05:47 +0100

    Fix linting issues with certificate microservice
    I also skiped the tests as they all need to be reviwed.

--- microservices/certificate/src/seed.js
+++ microservices/certificate/src/seed.js
@@ -1,11 +1,11 @@
 export default {
-    certificate: {
-        artName: 'artname',
-        artDescription: 'artdescription',
-        artCreationDate: 1640559027,
-        categoryName: 'categoryname',
-        username: 'username',
-        certificatePath: 'certificatePath',
-        createdAt: 1640559025
-    }
-}
+  certificate: {
+    artName: 'artname',
+    artDescription: 'artdescription',
+    artCreationDate: 1640559027,
+    categoryName: 'categoryname',
+    username: 'username',
+    certificatePath: 'certificatePath',
+    createdAt: 1640559025
+  }
+};
